5b9a894a6b3a4eedccec2ef5e35435d52b071d6c
hongbozheng <bobzheng258@gmail.com>
2017-01-26 12:21:13 -0800

    push my learning project of twilio video chat

---
+++ config.php
@@ -0,0 +1,7 @@
+
+<?php
+// see README.md for an explanation of these config params
+$TWILIO_ACCOUNT_SID = 'AC15c40780f87da5af9e5e2b925f7107cd';
+$TWILIO_CONFIGURATION_SID = 'VS8ed464e2a1516e397dc659b8e81429da';
+$TWILIO_API_KEY = 'SK4f559a9b6d2dfaf0eaf9aad59c23f0bd';
+$TWILIO_API_SECRET = 'BGVrKNyAFR3mNnLr8YpQn7nmKH7n6udN';
